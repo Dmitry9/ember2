be0ff87e1b9fe92127619acb4bed0d4287dc3c00
Dmitry9 <Karpov.Dmitry80@gmail.com>
2017-06-04 23:32:42 +0300

    added router

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -2,5 +2,14 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 	name: "Dmitry",
-	showName: false
+	showName: false,
+	numClicks: 0,
+	actions: {
+		toggleName() {
+			this.toggleProperty('showName');
+		},
+		increment() {
+			this.set('numClicks', this.get('numClicks') + 1);
+		}
+	}
 });
